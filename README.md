3285fbdd2f3f5f6b93c08d1ed4c98e4f86c65221
gorbe2002 <gorbe54@hotmail.com>
2025-12-18 14:14:45 -0500

    added background & basic Player class

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Route Skrtty-Six
 2D Driving Game
+
+# Artwork
+The artwork (photos in graphics folder) were made on Pixilart.com.
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pygame
 from sys import exit
 
+# classes
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("graphics/player/f1car.png").convert_alpha()
+        self.rect = self.image.get_rect(midbottom = (250,500))
+
 # game setup
 pygame.init()
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Route Skrtty-Six")
 clock = pygame.time.Clock() 
-game_active = False
+game_active = True # change to False when intro screen is added
+
+# Groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+# Background
+background_surf = pygame.image.load("graphics/background.png").convert()
 
 # game logic
 while True:
@@ -14,6 +28,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    if game_active:
+        screen.blit(background_surf, (0,0))
+
+        player.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
